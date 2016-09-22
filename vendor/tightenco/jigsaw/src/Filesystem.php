fe7015378b453ffec60a7e28cfdee5e280104037
Eric Peterson <eric@elpete.com>
2016-09-22 09:42:21 -0600

    Add initial scaffold for Jigsaw documentation site

---
+++ vendor/tightenco/jigsaw/src/Filesystem.php
@@ -0,0 +1,12 @@
+<?php namespace TightenCo\Jigsaw;
+
+use Illuminate\Filesystem\Filesystem as BaseFilesystem;
+use Symfony\Component\Finder\Finder;
+
+class Filesystem extends BaseFilesystem
+{
+    public function allFiles($directory, $hidden = false)
+    {
+        return iterator_to_array(Finder::create()->ignoreDotFiles($hidden)->files()->in($directory), false);
+    }
+}
