354a2cc65c71b7ccf2e1303f03ca4cdc6b8131a8
Eric Peterson <eric@elpete.com>
2016-09-29 14:44:25 -0600

    Fix links to other pages for integrated folder

--- docs/source/_partials/menu.blade.php
+++ docs/source/_partials/menu.blade.php
@@ -5,57 +5,57 @@
                 <aside class="menu">
                     <p class="menu-label">Assertion Methods</p>
                     <ul class="menu-list">
-                        <li><a href="/dontSee" class="{{ $page == '/dontSee' ? 'is-active' : '' }}">dontSee</a></li>
-                        <li><a href="/dontSeeElement" class="{{ $page == '/dontSeeElement' ? 'is-active' : '' }}">dontSeeElement</a></li>
-                        <li><a href="/dontSeeInCollection" class="{{ $page == '/dontSeeInCollection' ? 'is-active' : '' }}">dontSeeInCollection</a></li>
-                        <li><a href="/dontSeeInElement" class="{{ $page == '/dontSeeInElement' ? 'is-active' : '' }}">dontSeeInElement</a></li>
-                        <li><a href="/dontSeeInField" class="{{ $page == '/dontSeeInField' ? 'is-active' : '' }}">dontSeeInField</a></li>
-                        <li><a href="/dontSeeInTable" class="{{ $page == '/dontSeeInTable' ? 'is-active' : '' }}">dontSeeInTable</a></li>
-                        <li><a href="/dontSeeIsChecked" class="{{ $page == '/dontSeeIsChecked' ? 'is-active' : '' }}">dontSeeIsChecked</a></li>
-                        <li><a href="/dontSeeIsSelected" class="{{ $page == '/dontSeeIsSelected' ? 'is-active' : '' }}">dontSeeIsSelected</a></li>
-                        <li><a href="/dontSeeLink" class="{{ $page == '/dontSeeLink' ? 'is-active' : '' }}">dontSeeLink</a></li>
-                        <li><a href="/see" class="{{ $page == '/see' ? 'is-active' : '' }}">see</a></li>
-                        <li><a href="/seeActionIs" class="{{ $page == '/seeActionIs' ? 'is-active' : '' }}">seeActionIs</a></li>
-                        <li><a href="/seeControllerIs" class="{{ $page == '/seeControllerIs' ? 'is-active' : '' }}">seeControllerIs</a></li>
-                        <li><a href="/seeElement" class="{{ $page == '/seeElement' ? 'is-active' : '' }}">seeElement</a></li>
-                        <li><a href="/seeEventIs" class="{{ $page == '/seeEventIs' ? 'is-active' : '' }}">seeEventIs</a></li>
-                        <li><a href="/seeHandlerIs" class="{{ $page == '/seeHandlerIs' ? 'is-active' : '' }}">seeHandlerIs</a></li>
-                        <li><a href="/seeInCollection" class="{{ $page == '/seeInCollection' ? 'is-active' : '' }}">seeInCollection</a></li>
-                        <li><a href="/seeInElement" class="{{ $page == '/seeInElement' ? 'is-active' : '' }}">seeInElement</a></li>
-                        <li><a href="/seeInField" class="{{ $page == '/seeInField' ? 'is-active' : '' }}">seeInField</a></li>
-                        <li><a href="/seeInTable" class="{{ $page == '/seeInTable' ? 'is-active' : '' }}">seeInTable</a></li>
-                        <li><a href="/seeIsChecked" class="{{ $page == '/seeIsChecked' ? 'is-active' : '' }}">seeIsChecked</a></li>
-                        <li><a href="/seeIsSelected" class="{{ $page == '/seeIsSelected' ? 'is-active' : '' }}">seeIsSelected</a></li>
-                        <li><a href="/seeLink" class="{{ $page == '/seeLink' ? 'is-active' : '' }}">seeLink</a></li>
-                        <li><a href="/seePageIs" class="{{ $page == '/seePageIs' ? 'is-active' : '' }}">seePageIs</a></li>
-                        <li><a href="/seeTitleIs" class="{{ $page == '/seeTitleIs' ? 'is-active' : '' }}">seeTitleIs</a></li>
-                        <li><a href="/seeViewIs" class="{{ $page == '/seeViewIs' ? 'is-active' : '' }}">seeViewIs</a></li>
+                        <li><a href="/integrated/dontSee" class="{{ $page == '/dontSee' ? 'is-active' : '' }}">dontSee</a></li>
+                        <li><a href="/integrated/dontSeeElement" class="{{ $page == '/dontSeeElement' ? 'is-active' : '' }}">dontSeeElement</a></li>
+                        <li><a href="/integrated/dontSeeInCollection" class="{{ $page == '/dontSeeInCollection' ? 'is-active' : '' }}">dontSeeInCollection</a></li>
+                        <li><a href="/integrated/dontSeeInElement" class="{{ $page == '/dontSeeInElement' ? 'is-active' : '' }}">dontSeeInElement</a></li>
+                        <li><a href="/integrated/dontSeeInField" class="{{ $page == '/dontSeeInField' ? 'is-active' : '' }}">dontSeeInField</a></li>
+                        <li><a href="/integrated/dontSeeInTable" class="{{ $page == '/dontSeeInTable' ? 'is-active' : '' }}">dontSeeInTable</a></li>
+                        <li><a href="/integrated/dontSeeIsChecked" class="{{ $page == '/dontSeeIsChecked' ? 'is-active' : '' }}">dontSeeIsChecked</a></li>
+                        <li><a href="/integrated/dontSeeIsSelected" class="{{ $page == '/dontSeeIsSelected' ? 'is-active' : '' }}">dontSeeIsSelected</a></li>
+                        <li><a href="/integrated/dontSeeLink" class="{{ $page == '/dontSeeLink' ? 'is-active' : '' }}">dontSeeLink</a></li>
+                        <li><a href="/integrated/see" class="{{ $page == '/see' ? 'is-active' : '' }}">see</a></li>
+                        <li><a href="/integrated/seeActionIs" class="{{ $page == '/seeActionIs' ? 'is-active' : '' }}">seeActionIs</a></li>
+                        <li><a href="/integrated/seeControllerIs" class="{{ $page == '/seeControllerIs' ? 'is-active' : '' }}">seeControllerIs</a></li>
+                        <li><a href="/integrated/seeElement" class="{{ $page == '/seeElement' ? 'is-active' : '' }}">seeElement</a></li>
+                        <li><a href="/integrated/seeEventIs" class="{{ $page == '/seeEventIs' ? 'is-active' : '' }}">seeEventIs</a></li>
+                        <li><a href="/integrated/seeHandlerIs" class="{{ $page == '/seeHandlerIs' ? 'is-active' : '' }}">seeHandlerIs</a></li>
+                        <li><a href="/integrated/seeInCollection" class="{{ $page == '/seeInCollection' ? 'is-active' : '' }}">seeInCollection</a></li>
+                        <li><a href="/integrated/seeInElement" class="{{ $page == '/seeInElement' ? 'is-active' : '' }}">seeInElement</a></li>
+                        <li><a href="/integrated/seeInField" class="{{ $page == '/seeInField' ? 'is-active' : '' }}">seeInField</a></li>
+                        <li><a href="/integrated/seeInTable" class="{{ $page == '/seeInTable' ? 'is-active' : '' }}">seeInTable</a></li>
+                        <li><a href="/integrated/seeIsChecked" class="{{ $page == '/seeIsChecked' ? 'is-active' : '' }}">seeIsChecked</a></li>
+                        <li><a href="/integrated/seeIsSelected" class="{{ $page == '/seeIsSelected' ? 'is-active' : '' }}">seeIsSelected</a></li>
+                        <li><a href="/integrated/seeLink" class="{{ $page == '/seeLink' ? 'is-active' : '' }}">seeLink</a></li>
+                        <li><a href="/integrated/seePageIs" class="{{ $page == '/seePageIs' ? 'is-active' : '' }}">seePageIs</a></li>
+                        <li><a href="/integrated/seeTitleIs" class="{{ $page == '/seeTitleIs' ? 'is-active' : '' }}">seeTitleIs</a></li>
+                        <li><a href="/integrated/seeViewIs" class="{{ $page == '/seeViewIs' ? 'is-active' : '' }}">seeViewIs</a></li>
                     </ul>
                     <p class="menu-label">Interaction Methods</p>
                     <ul class="menu-list">
-                        <li><a href="/check" class="{{ $page == '/check' ? 'is-active' : '' }}">check</a></li></li>
-                        <li><a href="/click" class="{{ $page == '/click' ? 'is-active' : '' }}">click</a></li></li>
-                        <li><a href="/makeRequest" class="{{ $page == '/makeRequest' ? 'is-active' : '' }}">makeRequest</a></li></li>
-                        <li><a href="/press" class="{{ $page == '/press' ? 'is-active' : '' }}">press</a></li></li>
-                        <li><a href="/select" class="{{ $page == '/select' ? 'is-active' : '' }}">select</a></li></li>
-                        <li><a href="/submitForm" class="{{ $page == '/submitForm' ? 'is-active' : '' }}">submitForm</a></li></li>
-                        <li><a href="/type" class="{{ $page == '/type' ? 'is-active' : '' }}">type</a></li></li>
-                        <li><a href="/uncheck" class="{{ $page == '/uncheck' ? 'is-active' : '' }}">uncheck</a></li></li>
-                        <li><a href="/visit" class="{{ $page == '/visit' ? 'is-active' : '' }}">visit</a></li></li>
-                        <li><a href="/visitEvent" class="{{ $page == '/visitEvent' ? 'is-active' : '' }}">visitEvent</a></li></li>
+                        <li><a href="/integrated/check" class="{{ $page == '/check' ? 'is-active' : '' }}">check</a></li></li>
+                        <li><a href="/integrated/click" class="{{ $page == '/click' ? 'is-active' : '' }}">click</a></li></li>
+                        <li><a href="/integrated/makeRequest" class="{{ $page == '/makeRequest' ? 'is-active' : '' }}">makeRequest</a></li></li>
+                        <li><a href="/integrated/press" class="{{ $page == '/press' ? 'is-active' : '' }}">press</a></li></li>
+                        <li><a href="/integrated/select" class="{{ $page == '/select' ? 'is-active' : '' }}">select</a></li></li>
+                        <li><a href="/integrated/submitForm" class="{{ $page == '/submitForm' ? 'is-active' : '' }}">submitForm</a></li></li>
+                        <li><a href="/integrated/type" class="{{ $page == '/type' ? 'is-active' : '' }}">type</a></li></li>
+                        <li><a href="/integrated/uncheck" class="{{ $page == '/uncheck' ? 'is-active' : '' }}">uncheck</a></li></li>
+                        <li><a href="/integrated/visit" class="{{ $page == '/visit' ? 'is-active' : '' }}">visit</a></li></li>
+                        <li><a href="/integrated/visitEvent" class="{{ $page == '/visitEvent' ? 'is-active' : '' }}">visitEvent</a></li></li>
                     </ul>
                     <p class="menu-label">Debug Methods</p>
                     <ul class="menu-list">
-                        <li><a href="/debugPage" class="{{ $page == '/debugPage' ? 'is-active' : '' }}">debugPage</a></li></li>
-                        <li><a href="/debugEvent" class="{{ $page == '/debugEvent' ? 'is-active' : '' }}">debugEvent</a></li></li>
-                        <li><a href="/debugCollection" class="{{ $page == '/debugCollection' ? 'is-active' : '' }}">debugCollection</a></li></li>
-                        <li><a href="/debugPrivateCollection" class="{{ $page == '/debugPrivateCollection' ? 'is-active' : '' }}">debugPrivateCollection</a></li></li>
+                        <li><a href="/integrated/debugPage" class="{{ $page == '/debugPage' ? 'is-active' : '' }}">debugPage</a></li></li>
+                        <li><a href="/integrated/debugEvent" class="{{ $page == '/debugEvent' ? 'is-active' : '' }}">debugEvent</a></li></li>
+                        <li><a href="/integrated/debugCollection" class="{{ $page == '/debugCollection' ? 'is-active' : '' }}">debugCollection</a></li></li>
+                        <li><a href="/integrated/debugPrivateCollection" class="{{ $page == '/debugPrivateCollection' ? 'is-active' : '' }}">debugPrivateCollection</a></li></li>
                     </ul>
                     <p class="menu-label">Extending Integrated</p>
                     <ul class="menu-list">
-                        <li><a href="/under-the-hood" class="{{ $page == '/under-the-hood' ? 'is-active' : '' }}">Under the Hood</a></li></li>
-                        <li><a href="/custom-engines" class="{{ $page == '/custom-engines' ? 'is-active' : '' }}">Custom Engines</a></li></li>
-                        <li><a href="/custom-base-specs" class="{{ $page == '/writing-custom-base-specs' ? 'is-active' : '' }}">Custom Base Specs</a></li></li>
+                        <li><a href="/integrated/under-the-hood" class="{{ $page == '/under-the-hood' ? 'is-active' : '' }}">Under the Hood</a></li></li>
+                        <li><a href="/integrated/custom-engines" class="{{ $page == '/custom-engines' ? 'is-active' : '' }}">Custom Engines</a></li></li>
+                        <li><a href="/integrated/custom-base-specs" class="{{ $page == '/writing-custom-base-specs' ? 'is-active' : '' }}">Custom Base Specs</a></li></li>
                     </ul>
                 </aside>
             </div>
